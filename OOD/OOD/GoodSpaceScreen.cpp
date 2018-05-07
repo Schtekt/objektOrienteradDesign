8f3975309bd02c8cd2c18f29f7249de2b2392757
jakkeez <Jacob.andersson96@gmail.com>
2018-05-07 12:14:22 +0200

    Unfinished GoodSpaceScreen

---
+++ OOD/OOD/GoodSpaceScreen.cpp
@@ -0,0 +1,15 @@
+#include "GoodSpaceScreen.h"
+
+bool GoodSpaceScreen::setTemperature()
+{
+	float temp = 0;
+	std::cout << "Enter a temprature for the good: ";
+	std::cin >> temp;
+
+	wh->getCurrentWarehouse()->getGoodSpaceHandler()->getCurrentGoodSpace()->
+	return false;
+}
+
+void GoodSpaceScreen::remove()
+{
+}
