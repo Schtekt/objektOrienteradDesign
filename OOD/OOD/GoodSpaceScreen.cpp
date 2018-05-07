96fbcfd61efa9ddca295670e1388aad1f769e241
jakkeez <Jacob.andersson96@gmail.com>
2018-05-07 14:27:55 +0200

    Changes made to GoodSpaceScreen, unfinished

--- OOD/OOD/GoodSpaceScreen.cpp
+++ OOD/OOD/GoodSpaceScreen.cpp
@@ -1,15 +1,18 @@
 #include "GoodSpaceScreen.h"
 
-bool GoodSpaceScreen::setTemperature()
+void GoodSpaceScreen::setTemperature()
 {
+
 	float temp = 0;
-	std::cout << "Enter a temprature for the good: ";
+	std::cout << "Enter a temprature for the goodspace: ";
 	std::cin >> temp;
 
-	wh->getCurrentWarehouse()->getGoodSpaceHandler()->getCurrentGoodSpace()->
-	return false;
+	wh->getCurrentWarehouse()->getGoodSpaceHandler()->getCurrentGoodSpace()->setTemp(temp);
 }
 
-void GoodSpaceScreen::remove()
+void GoodSpaceScreen::selectGood()
 {
+	int id = -1;
+	std::cout << "Enter id for the good you want to select";
+	std::cin >> id;
 }
